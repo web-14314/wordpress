6e971f83a6049bd5f5cb94bc0885ddbebd366aa8
buidinhtoan <61192304+buidinhtoan@users.noreply.github.com>
2020-02-24 20:30:15 +0700

    toan

---
+++ wordpress/wp-content/plugins/ap-mega-menu/inc/backend/sidebar-right.php
@@ -0,0 +1,12 @@
+<div class="right_sidebar">
+			<div class="wpmm-upgrade-banner">
+              <img src="<?php echo APMM_IMG_DIR;?>/banner-image-pro.png" alt="upgrade-banner-top">
+              <div class="wpmm-button-wrap-backend">
+                <a href="http://demo.accesspressthemes.com/wordpress-plugins/wp-mega-menu-pro/" class="wpmm-demo-btn" target="_blank">Demo</a>
+                <a href="https://1.envato.market/myNXD" target="_blank" class="wpmm-upgrade-btn">Upgrade</a>
+                <a href="https://accesspressthemes.com/wordpress-plugins/wp-mega-menu-pro/" target="_blank" class="wpmm-upgrade-btn">Plugin Information</a>
+              </div>
+            <img src="<?php echo APMM_IMG_DIR;?>/banner-feature-pro.png" alt="upgrade-banner-bottom">
+        </div>
+
+</div>
